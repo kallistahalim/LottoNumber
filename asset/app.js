7f25301f289f8f803e5aca4c3dde4dcb5b0254e2
kallistahalim <kallistahalim@gmail.com>
2020-04-21 23:12:44 -0700

    app works

--- asset/app.js
+++ asset/app.js
@@ -1,4 +1,14 @@
 $("#button").on("click", function () {
     console.log("button works");
+    var randomNumbers = "";
+
+    for (var i = 0; i < 9; i++) {
+        var randomNum = Math.floor(Math.random() * 9) + 1;
+        randomNumbers = randomNum + randomNumbers;
+        console.log(randomNumbers);
+    }
+
+    $("#randomNumbers").append("<br><hr>" + "<h1>" + randomNumbers + "</h1>");
+
 
 });
